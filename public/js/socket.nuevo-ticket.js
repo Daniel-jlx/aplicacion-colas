87a262d4af5e40dc6930712a2ae4b8562df3ea33
Daniel <dani.roj789@gmail.com>
2021-06-16 09:55:33 -0500

    terminado

---
+++ public/js/socket.nuevo-ticket.js
@@ -0,0 +1,39 @@
+// const { io } = require('../server');
+// const { TicketControl } = require('../classes/ticket-control');
+
+// const { emit } = require("nodemon");
+
+// Comando para establecer la conexion
+var socket = io();
+
+var label = $('#lblNuevoTicket');
+
+
+
+
+socket.on('connect', function() {
+    console.log('Conectado al servidor');
+});
+
+
+
+socket.on('disconnect', function() {
+    console.log('Desconectado del servidor');
+});
+
+socket.on('estadoActual', function(resp) {
+
+    console.log(resp);
+    label.text(resp.actual);
+
+});
+
+$('button').on('click', function() {
+
+    socket.emit('siguienteTicket', null, function(siguienteTicket) {
+
+        label.text(siguienteTicket);
+
+    });
+
+});
